44db9b1fd6e5286ed341f9884e32b0544037e2fa
Marcela Cunha <marcela.cunha@liferay.com>
2021-05-07 12:32:41 -0300

    LPS-131630 Create an interface to validation type

---
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-api/src/main/java/com/liferay/dynamic/data/mapping/validation/DDMValidationType.java
@@ -0,0 +1,34 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.dynamic.data.mapping.validation;
+
+/**
+ * @author Marcela Cunha
+ */
+public interface DDMValidationType {
+
+	public String getDataType();
+
+	public String getLabel();
+
+	public String getName();
+
+	public String getParameterMessage();
+
+	public String getRegex();
+
+	public String getTemplate();
+
+}
